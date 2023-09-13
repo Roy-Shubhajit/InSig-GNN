Fixed variance calculation of labels

dataset_creation.py
@@ -1,4 +1,6 @@
 
+from torch_geometric.utils import degree
+import torch.utils.data
 import warnings
 from torch_geometric.data import Data, InMemoryDataset, download_url, Batch
 from dgl.data.utils import Subset, load_graphs
@@ -6,8 +8,6 @@ import torch
 from torch_geometric.utils import k_hop_subgraph
 from scipy.special import comb
 warnings.filterwarnings('ignore')
-import torch.utils.data
-from torch_geometric.utils import degree
 
 
 class Dataset_1_orig(InMemoryDataset):
@@ -35,13 +35,13 @@ class Dataset_1_orig(InMemoryDataset):
         num_edges = 0
         for ind in node_name:
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind.item(), 1, edge_index, False, num_nodes)
+                ind.item(), 1, edge_index, False, num_nodes)
             edge_attr_ = None
             edge_index_ = edge_index_.T
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
             num_edges += edge_index_.shape[1]
-        return torch.tensor([num_edges//6]) 
+        return torch.tensor([num_edges//6])
 
     def count_K4(self, data):
         edge_index, num_nodes = data.edge_index, data.num_nodes
@@ -49,7 +49,7 @@ class Dataset_1_orig(InMemoryDataset):
         l = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 2, edge_index, False, num_nodes)
+                ind, 2, edge_index, False, num_nodes)
             edge_index_ = edge_index_.T
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
@@ -63,11 +63,12 @@ class Dataset_1_orig(InMemoryDataset):
         k = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 1, egde_index, False, num_nodes)
+                ind, 1, egde_index, False, num_nodes)
             edge_index_ = edge_index_.T
             mask = (edge_index_ == ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
-            k = torch.cat((k,torch.tensor([comb(nodes_.shape[0]-1,2, exact=True)])), dim=0)
+            k = torch.cat(
+                (k, torch.tensor([comb(nodes_.shape[0]-1, 2, exact=True)])), dim=0)
         return torch.sum(k)
 
     def count_chordal(self, data):
@@ -76,43 +77,45 @@ class Dataset_1_orig(InMemoryDataset):
         l = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 1, edge_index, False, num_nodes)
+                ind, 1, edge_index, False, num_nodes)
             edge_index_ = edge_index_.T
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
-            total_edge_index = torch.cat((total_edge_index, edge_index_.T), dim=0)
+            total_edge_index = torch.cat(
+                (total_edge_index, edge_index_.T), dim=0)
             nodes_ = nodes_[nodes_ != ind]
             deg = degree(edge_index_[0], num_nodes)
-            ll = sum([comb(i, 2, exact=True) for i in  deg[deg > 1]])
+            ll = sum([comb(i, 2, exact=True) for i in deg[deg > 1]])
             l = torch.cat((l, torch.tensor([ll])), dim=0)
         return torch.sum(l)//2
-    
+
     def countC4(self, data):
         edge_index, num_nodes = data.edge_index, data.num_nodes
         l = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             node_dict = {}
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 2, edge_index, False, num_nodes)
+                ind, 2, edge_index, False, num_nodes)
             edge_index_ = edge_index_.T
             nodes_ = nodes_[nodes_ != ind]
             edge_list = edge_index_.tolist()
-            node_dict = {n.item(): 1 if [ind, n] in edge_list or [n, ind] in edge_list else 0 for n in nodes_}
+            node_dict = {n.item(): 1 if [ind, n] in edge_list or [
+                n, ind] in edge_list else 0 for n in nodes_}
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
             ll = 0
             edge_list = edge_index_.T.tolist()
 
             for n in node_dict:
-                nei = sum([1 for m in node_dict if [n, m] in edge_list and [m, n] in edge_list and node_dict[m] == 1])
+                nei = sum([1 for m in node_dict if [n, m] in edge_list and [
+                          m, n] in edge_list and node_dict[m] == 1])
                 if nei >= 2:
                     ll += comb(nei, 2, exact=True)
-                    
+
             l = torch.cat((l, torch.tensor([ll])), dim=0)
-       
-        
+
         return torch.ceil(torch.sum(l)/4)
-    
+
     def from_dgl(self, g, star, tri, tail_tri, attr_tri, chord):
         import dgl
 
@@ -121,7 +124,6 @@ class Dataset_1_orig(InMemoryDataset):
         if not isinstance(g, dgl.DGLGraph):
             raise ValueError(f"Invalid data type (got '{type(g)}')")
 
-       
         data = Data()
         data.edge_index = torch.stack(g.edges(), dim=0)
 
@@ -141,7 +143,8 @@ class Dataset_1_orig(InMemoryDataset):
         return data
 
     def process(self):
-        glist, all_labels = load_graphs("/hdfs1/Data/Shubhajit/WL_Substructure_Counting/data/dataset1.bin")
+        glist, all_labels = load_graphs(
+            "/hdfs1/Data/Shubhajit/WL_Substructure_Counting/data/dataset1.bin")
         data_list = []
         for i in zip(glist, all_labels["star"], all_labels["triangle"], all_labels["tailed_triangle"], all_labels["attributed_triangle"], all_labels["chordal_cycle"]):
             data = self.from_dgl(i[0], i[1], i[2], i[3], i[4], i[5])
@@ -152,6 +155,7 @@ class Dataset_1_orig(InMemoryDataset):
 
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
+
 
 class Dataset_2_orig(InMemoryDataset):
     def __init__(self, root, transform=None, pre_transform=None):
@@ -178,13 +182,13 @@ class Dataset_2_orig(InMemoryDataset):
         num_edges = 0
         for ind in node_name:
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind.item(), 1, edge_index, False, num_nodes)
+                ind.item(), 1, edge_index, False, num_nodes)
             edge_attr_ = None
             edge_index_ = edge_index_.T
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
             num_edges += edge_index_.shape[1]
-        return torch.tensor([num_edges//6]) 
+        return torch.tensor([num_edges//6])
 
     def count_K4(self, data):
         edge_index, num_nodes = data.edge_index, data.num_nodes
@@ -192,7 +196,7 @@ class Dataset_2_orig(InMemoryDataset):
         l = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 2, edge_index, False, num_nodes)
+                ind, 2, edge_index, False, num_nodes)
             edge_index_ = edge_index_.T
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
@@ -206,11 +210,12 @@ class Dataset_2_orig(InMemoryDataset):
         k = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 1, egde_index, False, num_nodes)
+                ind, 1, egde_index, False, num_nodes)
             edge_index_ = edge_index_.T
             mask = (edge_index_ == ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
-            k = torch.cat((k,torch.tensor([comb(nodes_.shape[0]-1,2, exact=True)])), dim=0)
+            k = torch.cat(
+                (k, torch.tensor([comb(nodes_.shape[0]-1, 2, exact=True)])), dim=0)
         return torch.sum(k)
 
     def count_chordal(self, data):
@@ -219,14 +224,15 @@ class Dataset_2_orig(InMemoryDataset):
         l = torch.tensor([], dtype=torch.long)
         for ind in range(num_nodes):
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 1, edge_index, False, num_nodes)
+                ind, 1, edge_index, False, num_nodes)
             edge_index_ = edge_index_.T
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
-            total_edge_index = torch.cat((total_edge_index, edge_index_.T), dim=0)
+            total_edge_index = torch.cat(
+                (total_edge_index, edge_index_.T), dim=0)
             nodes_ = nodes_[nodes_ != ind]
             deg = degree(edge_index_[0], num_nodes)
-            ll = sum([comb(i, 2, exact=True) for i in  deg[deg > 1]])
+            ll = sum([comb(i, 2, exact=True) for i in deg[deg > 1]])
             l = torch.cat((l, torch.tensor([ll])), dim=0)
         return torch.sum(l)//2
 
@@ -236,24 +242,25 @@ class Dataset_2_orig(InMemoryDataset):
         for ind in range(num_nodes):
             node_dict = {}
             nodes_, edge_index_, edge_mask_, z_ = k_hop_subgraph(
-            ind, 2, edge_index, False, num_nodes)
+                ind, 2, edge_index, False, num_nodes)
             edge_index_ = edge_index_.T
             nodes_ = nodes_[nodes_ != ind]
             edge_list = edge_index_.tolist()
-            node_dict = {n.item(): 1 if [ind, n] in edge_list or [n, ind] in edge_list else 0 for n in nodes_}
+            node_dict = {n.item(): 1 if [ind, n] in edge_list or [
+                n, ind] in edge_list else 0 for n in nodes_}
             mask = (edge_index_ != ind).all(dim=1)
             edge_index_ = edge_index_[mask].T
             ll = 0
             edge_list = edge_index_.T.tolist()
 
             for n in node_dict:
-                nei = sum([1 for m in node_dict if [n, m] in edge_list and [m, n] in edge_list and node_dict[m] == 1])
+                nei = sum([1 for m in node_dict if [n, m] in edge_list and [
+                          m, n] in edge_list and node_dict[m] == 1])
                 if nei >= 2:
                     ll += comb(nei, 2, exact=True)
-                    
+
             l = torch.cat((l, torch.tensor([ll])), dim=0)
-       
-        
+
         return torch.ceil(torch.sum(l)/4)
 
     def from_dgl(self, g, star, tri, tail_tri, attr_tri, chord):
@@ -264,7 +271,6 @@ class Dataset_2_orig(InMemoryDataset):
         if not isinstance(g, dgl.DGLGraph):
             raise ValueError(f"Invalid data type (got '{type(g)}')")
 
-        
         data = Data()
         data.edge_index = torch.stack(g.edges(), dim=0)
 
@@ -284,7 +290,8 @@ class Dataset_2_orig(InMemoryDataset):
         return data
 
     def process(self):
-        glist, all_labels = load_graphs("/hdfs1/Data/Shubhajit/WL_Substructure_Counting/data/dataset2.bin")
+        glist, all_labels = load_graphs(
+            "/hdfs1/Data/Shubhajit/WL_Substructure_Counting/data/dataset2.bin")
         data_list = []
         for i in zip(glist, all_labels["star"], all_labels["triangle"], all_labels["tailed_triangle"], all_labels["attributed_triangle"], all_labels["chordal_cycle"]):
             data = self.from_dgl(i[0], i[1], i[2], i[3], i[4], i[5])

fragmentation_K4.py
@@ -8,8 +8,10 @@ import os
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 
 def triangle_count(tri_int, tri_ext, graph, subgraph, max_nodes):
     tri_int.eval()
@@ -24,6 +26,7 @@ def triangle_count(tri_int, tri_ext, graph, subgraph, max_nodes):
     ext_emb = ext_emb.float()
     return ext_emb
 
+
 def train(args, int_model, ext_model, predictor, loader, pred_opt, loss_fn1):
     step = 0
     total_loss_int = 0
@@ -33,22 +36,23 @@ def train(args, int_model, ext_model, predictor, loader, pred_opt, loss_fn1):
         subgraphs = batch[1]
         subsubgraphs = batch[4]
         for g in range(len(graphs)):
-                graphs[g] = graphs[g].to(device)
-                for key in subgraphs[g].keys():
-                    subgraphs[g][key] = subgraphs[g][key].to(device)
-                    for j in subsubgraphs[g][key]:
-                        for n in j.keys():
-                            j[n] = j[n].to(device)
-        
+            graphs[g] = graphs[g].to(device)
+            for key in subgraphs[g].keys():
+                subgraphs[g][key] = subgraphs[g][key].to(device)
+                for j in subsubgraphs[g][key]:
+                    for n in j.keys():
+                        j[n] = j[n].to(device)
+
         labels = batch[2].to(device)
         max_nodes = batch[3]
         subgraph_max_nodes = batch[5]
         count = torch.tensor(0.0).to(device)
         for j in range(len(subgraphs)):
             for k in subgraphs[j].keys():
-                m = triangle_count(int_model, ext_model, [subgraphs[j][k]], subsubgraphs[j][k], subgraph_max_nodes[j])
+                m = triangle_count(int_model, ext_model, [
+                                   subgraphs[j][k]], subsubgraphs[j][k], subgraph_max_nodes[j])
                 count = count + m
-                    
+
         pred_opt.zero_grad()
         pred = predictor(count)
         loss = loss_fn1(pred, graphs[0].ext_label)
@@ -59,9 +63,9 @@ def train(args, int_model, ext_model, predictor, loader, pred_opt, loss_fn1):
         if step % args.step == 0:
             print("Step: {}, Loss: {}".format(
                 step, total_loss_int/step))
-            
+
     return total_loss_int / step
-        
+
 
 def eval(args, int_model, ext_model, predictor, loader, loss_fn1):
     step = 0
@@ -72,13 +76,12 @@ def eval(args, int_model, ext_model, predictor, loader, loss_fn1):
         subgraphs = batch[1]
         subsubgraphs = batch[4]
         for g in range(len(graphs)):
-                graphs[g] = graphs[g].to(device)
-                for key in subgraphs[g].keys():
-                    subgraphs[g][key] = subgraphs[g][key].to(device)
-                    for j in subsubgraphs[g][key]:
-                        for n in j.keys():
-                            j[n] = j[n].to(device)
-        
+            graphs[g] = graphs[g].to(device)
+            for key in subgraphs[g].keys():
+                subgraphs[g][key] = subgraphs[g][key].to(device)
+                for j in subsubgraphs[g][key]:
+                    for n in j.keys():
+                        j[n] = j[n].to(device)
 
         labels = batch[2].to(device)
         max_nodes = batch[3]
@@ -86,7 +89,8 @@ def eval(args, int_model, ext_model, predictor, loader, loss_fn1):
         count = torch.tensor(0.0).to(device)
         for j in range(len(subgraphs)):
             for k in subgraphs[j].keys():
-                m = triangle_count(int_model, ext_model, [subgraphs[j][k]], subsubgraphs[j][k], subgraph_max_nodes[j])
+                m = triangle_count(int_model, ext_model, [
+                                   subgraphs[j][k]], subsubgraphs[j][k], subgraph_max_nodes[j])
                 count = count + m
         pred = predictor(count)
         pred = new_round(pred, 0.5)
@@ -96,6 +100,7 @@ def eval(args, int_model, ext_model, predictor, loader, loss_fn1):
         total_loss += loss.item()
         step += 1
     return total_loss / step
+
 
 def main(args):
 
@@ -113,28 +118,42 @@ def main(args):
         val_dataset = dataset[int(len(dataset)*0.8):int(len(dataset)*0.9)]
         test_dataset = dataset[int(len(dataset)*0.9):]
 
-    variance = torch.std(dataset.data.K4.to(torch.float))**2
-
-
-    print("Variance: ", variance)
-
     collater_fn = frag_collater_K4()
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collater_fn)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collater_fn)
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collater_fn)
+    train_loader = DataLoader(
+        train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=collater_fn)
+    val_loader = DataLoader(
+        val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collater_fn)
+    test_loader = DataLoader(
+        test_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=collater_fn)
 
     int_triangle = localGNN_K4(1, 512).to(device)
     ext_triangle = new_external(1, 64).to(device)
 
-    int_triangle.load_state_dict(torch.load('/hdfs1/Data/Shubhajit/Sub-Structure-GNN/save/triangle_insig/Int_GNN_triangle_dataset_2.pt'))
-    ext_triangle.load_state_dict(torch.load('/hdfs1/Data/Shubhajit/Sub-Structure-GNN/save/triangle_insig/Ext_GNN_triangle_dataset_2.pt'))
+    int_triangle.load_state_dict(torch.load(
+        '/hdfs1/Data/Shubhajit/Sub-Structure-GNN/save/triangle_insig/Int_GNN_triangle_dataset_2.pt'))
+    ext_triangle.load_state_dict(torch.load(
+        '/hdfs1/Data/Shubhajit/Sub-Structure-GNN/save/triangle_insig/Ext_GNN_triangle_dataset_2.pt'))
 
-    predict_model = predictor(1, 64).to(device) 
+    predict_model = predictor(1, 64).to(device)
 
     pred_opt = torch.optim.Adam(predict_model.parameters(), lr=args.lr)
     loss_fn1 = torch.nn.L1Loss(reduction='mean')
 
-    print("Number of parameters in predictor: ", count_parameters(predict_model))
+    labels = torch.tensor([]).to(device)
+    for batch in tqdm(train_loader):
+        labels = torch.cat(
+            (labels, torch.sum(batch[2]).reshape(1).to(device)), 0)
+    for batch in tqdm(val_loader):
+        labels = torch.cat(
+            (labels, torch.sum(batch[2]).reshape(1).to(device)), 0)
+    for batch in tqdm(test_loader):
+        labels = torch.cat(
+            (labels, torch.sum(batch[2]).reshape(1).to(device)), 0)
+    variance = torch.std(labels)**2
+
+    print("Variance: ", variance.item())
+    print("Number of parameters in predictor: ",
+          count_parameters(predict_model))
 
     train_curve = []
     valid_curve = []
@@ -144,13 +163,16 @@ def main(args):
     for epoch in range(1, args.epochs+1):
         print("=====Epoch {}".format(epoch))
         print('Training...')
-        train_loss = train(args, int_triangle, ext_triangle, predict_model, train_loader, pred_opt, loss_fn1)
+        train_loss = train(args, int_triangle, ext_triangle,
+                           predict_model, train_loader, pred_opt, loss_fn1)
         print('Train loss : {}'.format(train_loss/variance))
 
         print('Evaluating...')
-        valid_loss = eval(args, int_triangle, ext_triangle, predict_model, val_loader, loss_fn1)
+        valid_loss = eval(args, int_triangle, ext_triangle,
+                          predict_model, val_loader, loss_fn1)
         print('Valid loss : {}'.format(valid_loss/variance))
-        test_loss = eval(args, int_triangle, ext_triangle, predict_model, test_loader, loss_fn1)
+        test_loss = eval(args, int_triangle, ext_triangle,
+                         predict_model, test_loader, loss_fn1)
         print('Test loss : {}'.format(test_loss/variance))
         if valid_loss <= best_val_loss:
             if valid_loss == best_val_loss:
@@ -159,17 +181,20 @@ def main(args):
                     best_test_loss = test_loss
                     print("Best Model!")
                     print("Best valid loss : {}".format(best_val_loss/variance))
-                    print("Best test loss : {}".format(best_test_loss/variance))
-                    #save the best model
-                    torch.save(predict_model.state_dict(), "save/{}/predict_model_{}.pt".format(args.output_file, args.dataset))
+                    print("Best test loss : {}".format(
+                        best_test_loss/variance))
+                    # save the best model
+                    torch.save(predict_model.state_dict(
+                    ), "save/{}/predict_model_{}.pt".format(args.output_file, args.dataset))
             else:
                 best_val_loss = valid_loss
                 best_test_loss = test_loss
                 print("Best Model!")
                 print("Best valid loss : {}".format(best_val_loss/variance))
                 print("Best test loss : {}".format(best_test_loss/variance))
-                #save the best model
-                torch.save(predict_model.state_dict(), "save/{}/predict_model_{}.pt".format(args.output_file, args.dataset))
+                # save the best model
+                torch.save(predict_model.state_dict(
+                ), "save/{}/predict_model_{}.pt".format(args.output_file, args.dataset))
         train_curve.append(train_loss/variance)
         valid_curve.append(valid_loss/variance)
         test_curve.append(test_loss/variance)
@@ -177,12 +202,17 @@ def main(args):
     print("Best valid loss : {}".format(best_val_loss/variance))
     print("Best test loss : {}".format(best_test_loss/variance))
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Sub-Structure GNN')
-    parser.add_argument('--dataset', type=str, default='dataset_2', help='Dataset: dataset_1 or dataset_2')
-    parser.add_argument('--epochs', type=int, default=100, help='Number of epochs')
-    parser.add_argument('--output_file', type=str, default='triangle_2', help='Output file name')
-    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
+    parser.add_argument('--dataset', type=str, default='dataset_2',
+                        help='Dataset: dataset_1 or dataset_2')
+    parser.add_argument('--epochs', type=int, default=100,
+                        help='Number of epochs')
+    parser.add_argument('--output_file', type=str,
+                        default='triangle_2', help='Output file name')
+    parser.add_argument('--lr', type=float, default=0.0001,
+                        help='Learning rate')
     parser.add_argument('--batch_size', type=int, default=1, help='Batch size')
     parser.add_argument('--step', type=int, default=500)
     args = parser.parse_args()

helper.py
@@ -414,7 +414,7 @@ class frag_collater_tailed_triangle():
         total_edge_index = torch.unique(total_edge_index, dim=0)
 
         new_data = Data(edge_index=total_edge_index.T)
-        new_data.ext_label = torch.sum(l)
+        new_data.ext_label = l
         new_data.ext_label_dataset = data.tailed_triangle
         return new_data, subgraphs, l, subsubgraphs, max([d.num_nodes for d in subgraphs.values()])
 

main.py
@@ -9,8 +9,10 @@ import os
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 
 def train(int_model, ext_model, loader, int_opt, ext_opt, args):
     step = 0
@@ -21,7 +23,7 @@ def train(int_model, ext_model, loader, int_opt, ext_opt, args):
         subgraphs = batch[1]
         internal_labels = batch[2].to(device)
         max_nodes = batch[3]
-        
+
         for g in range(len(graphs)):
             graphs[g] = graphs[g].to(device)
             for key in subgraphs[g].keys():
@@ -55,6 +57,7 @@ def train(int_model, ext_model, loader, int_opt, ext_opt, args):
                 step, total_loss_int/step, total_loss_ext/step))
     return total_loss_ext / step
 
+
 def eval(int_model, ext_model, loader, args):
     int_model.eval()
     ext_model.eval()
@@ -65,7 +68,7 @@ def eval(int_model, ext_model, loader, args):
         subgraphs = batch[1]
         internal_labels = batch[2].to(device)
         max_nodes = batch[3]
-        
+
         for g in range(len(graphs)):
             graphs[g] = graphs[g].to(device)
             for key in subgraphs[g].keys():
@@ -86,6 +89,7 @@ def eval(int_model, ext_model, loader, args):
         step += 1
     return total_loss / step
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Sub-Structure GNN')
     parser.add_argument('--task', type=str, default='triangle')
@@ -102,7 +106,6 @@ if __name__ == '__main__':
     if 'save/'+args.output_file not in os.listdir():
         os.mkdir('save/'+args.output_file)
 
-
     if args.dataset == 'dataset_1':
         dataset = Dataset_1_orig(root="data/Dataset1", pre_transform=None)
         train_dataset = dataset[:int(len(dataset)*0.8)]
@@ -113,26 +116,14 @@ if __name__ == '__main__':
         train_dataset = dataset[:int(len(dataset)*0.8)]
         val_dataset = dataset[int(len(dataset)*0.8):int(len(dataset)*0.9)]
         test_dataset = dataset[int(len(dataset)*0.9):]
-    
-    if args.task == 'triangle':
-        variance = torch.std(dataset.data.triangle.to(torch.float32))**2
-    elif args.task == '3star':
-        variance = torch.std(dataset.data.star.to(torch.float32))**2
-    elif args.task == 'chordal':
-        variance = torch.std(dataset.data.chordal_cycle.to(torch.float32))**2
-    elif args.task == '2star':
-        variance = torch.std(dataset.data.star_2.to(torch.float32))**2
-    elif args.task == "local_nodes":
-        variance = torch.tensor(1.0)    
-    elif args.task == "local_edges":
-        variance = torch.tensor(1.0)    
-        
-    print("Variance: ", variance)
 
     collater_fn = collater(args.task)
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, collate_fn=collater_fn)
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True, collate_fn=collater_fn)
-    test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, drop_last=True, collate_fn=collater_fn)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size,
+                              shuffle=True, drop_last=True, collate_fn=collater_fn)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size,
+                            shuffle=False, drop_last=True, collate_fn=collater_fn)
+    test_loader = DataLoader(test_dataset, batch_size=args.batch_size,
+                             shuffle=False, drop_last=True, collate_fn=collater_fn)
 
     Int_GNN = localGNN(1, 512).to(device)
     if args.model == 'insig':
@@ -144,18 +135,33 @@ if __name__ == '__main__':
     loss_fn1 = torch.nn.L1Loss(reduction='mean')
     loss_fn2 = torch.nn.L1Loss(reduction='mean')
 
-    print("Number of parameters in Int_GNN: ", count_parameters(Int_GNN))
-    print("Number of parameters in Ext_GNN: ", count_parameters(Ext_GNN))
-
     train_curve = []
     valid_curve = []
     test_curve = []
     best_val_loss = 1000
     best_test_loss = 1000
+
+    labels = torch.tensor([]).to(device)
+    for batch in tqdm(train_loader):
+        labels = torch.cat(
+            (labels, torch.sum(batch[2]).reshape(1).to(device)), 0)
+    for batch in tqdm(val_loader):
+        labels = torch.cat(
+            (labels, torch.sum(batch[2]).reshape(1).to(device)), 0)
+    for batch in tqdm(test_loader):
+        labels = torch.cat(
+            (labels, torch.sum(batch[2]).reshape(1).to(device)), 0)
+    variance = torch.std(labels)**2
+
+    print("Variance: ", variance.item())
+    print("Number of parameters in Int_GNN: ", count_parameters(Int_GNN))
+    print("Number of parameters in Ext_GNN: ", count_parameters(Ext_GNN))
+
     for epoch in range(1, args.epochs+1):
         print("=====Epoch {}".format(epoch))
         print('Training...')
-        train_loss = train(Int_GNN, Ext_GNN, train_loader, Int_Opt, Ext_Opt, args)
+        train_loss = train(Int_GNN, Ext_GNN, train_loader,
+                           Int_Opt, Ext_Opt, args)
         print('Train loss : {}'.format(train_loss/variance))
 
         print('Evaluating...')
@@ -165,31 +171,33 @@ if __name__ == '__main__':
         print('Test loss : {}'.format(test_loss/variance))
         if valid_loss <= best_val_loss:
             if valid_loss == best_val_loss:
-                #only save if the test loss is lower
+                # only save if the test loss is lower
                 if test_loss < best_test_loss:
                     best_val_loss = valid_loss
                     best_test_loss = test_loss
                     print("Best Model!")
                     print("Best valid loss : {}".format(best_val_loss/variance))
-                    print("Best test loss : {}".format(best_test_loss/variance))
-                    #save the best model
-                    torch.save(Int_GNN.state_dict(), "save/{}/Int_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
-                    torch.save(Ext_GNN.state_dict(), "save/{}/Ext_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
+                    print("Best test loss : {}".format(
+                        best_test_loss/variance))
+                    # save the best model
+                    torch.save(Int_GNN.state_dict(
+                    ), "save/{}/Int_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
+                    torch.save(Ext_GNN.state_dict(
+                    ), "save/{}/Ext_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
             else:
                 best_val_loss = valid_loss
                 best_test_loss = test_loss
                 print("Best Model!")
                 print("Best valid loss : {}".format(best_val_loss/variance))
                 print("Best test loss : {}".format(best_test_loss/variance))
-                #save the best model
-                torch.save(Int_GNN.state_dict(), "save/{}/Int_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
-                torch.save(Ext_GNN.state_dict(), "save/{}/Ext_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
+                # save the best model
+                torch.save(Int_GNN.state_dict(
+                ), "save/{}/Int_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
+                torch.save(Ext_GNN.state_dict(
+                ), "save/{}/Ext_GNN_{}_{}.pt".format(args.output_file, args.task, args.dataset))
         train_curve.append(train_loss/variance)
         valid_curve.append(valid_loss/variance)
         test_curve.append(test_loss/variance)
 
     print("Best valid loss : {}".format(best_val_loss/variance))
     print("Best test loss : {}".format(best_test_loss/variance))
-
-    
-    

model.py
@@ -1,4 +1,5 @@
 
+import torch.utils.data
 import warnings
 from torch_geometric.data import Data, InMemoryDataset, download_url, Batch
 from dgl.data.utils import Subset, load_graphs
@@ -9,21 +10,9 @@ from torch.nn import Linear, Sequential, BatchNorm1d, ReLU, Dropout, ELU, GELU
 from torch_geometric.nn import GINConv, GINEConv, WLConv, GATv2Conv
 from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool
 warnings.filterwarnings('ignore')
-import torch.utils.data
 
 
-
-import warnings
-from torch_geometric.data import Data, InMemoryDataset, download_url, Batch
-from dgl.data.utils import Subset, load_graphs
-from torch_geometric.utils import k_hop_subgraph
-import torch
-import torch.nn.functional as F
-from torch.nn import Linear, Sequential, BatchNorm1d, ReLU, Dropout, ELU, GELU
-from torch_geometric.nn import GINConv, GINEConv, WLConv, GATv2Conv
-from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool
 warnings.filterwarnings('ignore')
-import torch.utils.data
 
 
 class localGNN(torch.nn.Module):
@@ -53,23 +42,26 @@ class localGNN(torch.nn.Module):
                 x = self.conv2(x, edge_index)
                 x = self.fc0(x)
                 x = F.gelu(x)
-                if len(x)>0:
-                    batch = torch.zeros(len(x), dtype=torch.long).to(data_list[0].edge_index.device)
+                if len(x) > 0:
+                    batch = torch.zeros(len(x), dtype=torch.long).to(
+                        data_list[0].edge_index.device)
                     x = global_add_pool(x, batch, size=1)
                 else:
-                    x = torch.zeros(1, self.hidden).to(data_list[0].edge_index.device)
+                    x = torch.zeros(1, self.hidden).to(
+                        data_list[0].edge_index.device)
                 emb = torch.cat((emb, x), dim=0)
             if emb.shape[0] == 0:
                 continue
             else:
                 int_emb[num, :emb.shape[0], :] = emb
             num += 1
-            graph.x = emb  
-            new_data_list.append(graph) 
+            graph.x = emb
+            new_data_list.append(graph)
         res = self.fc1(int_emb)
         batch = Batch.from_data_list(new_data_list)
         res = torch.squeeze(res, dim=-1)
         return batch, res
+
 
 class globalGNN(torch.nn.Module):
     def __init__(self, num_layers, hidden):
@@ -82,14 +74,14 @@ class globalGNN(torch.nn.Module):
         self.fc2 = Linear(hidden, 1)
 
     def forward(self, data):
-        
-        
+
         x = self.conv3(data.x, data.edge_index)
         x = self.conv4(x, data.edge_index)
         x = self.fc2(x)
         x = global_add_pool(x, data.batch)
         return x
-    
+
+
 class new_external(torch.nn.Module):
     def __init__(self, num_layer, hidden):
         super(new_external, self).__init__()
@@ -99,11 +91,12 @@ class new_external(torch.nn.Module):
 
     def forward(self, x):
         x = torch.sum(x, dim=1)
-        x = x.reshape(-1,1)
+        x = x.reshape(-1, 1)
         x = self.fc1(x)
         x = F.gelu(x)
         x = self.fc2(x)
         return x
+
 
 class predictor(torch.nn.Module):
     def __init__(self, input_emb, hidden):
@@ -118,7 +111,22 @@ class predictor(torch.nn.Module):
         x = F.gelu(x)
         x = self.fc2(x)
         return x
-    
+
+
+class predictor_tailed_triangle(torch.nn.Module):
+    def __init__(self, input_emb, hidden):
+        super(predictor_tailed_triangle, self).__init__()
+        self.hidden = hidden
+        self.fc1 = Linear(input_emb, hidden)
+        self.fc2 = Linear(hidden, 1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = F.gelu(x)
+        x = self.fc2(x)
+        return x
+
+
 class localGNN_K4(torch.nn.Module):
     def __init__(self, num_layers, hidden):
         super(localGNN_K4, self).__init__()
@@ -146,19 +154,21 @@ class localGNN_K4(torch.nn.Module):
                 x = self.conv2(x, edge_index)
                 x = self.fc0(x)
                 x = F.gelu(x)
-                if len(x)>0:
-                    batch = torch.zeros(len(x), dtype=torch.long).to(data_list[0].edge_index.device)
+                if len(x) > 0:
+                    batch = torch.zeros(len(x), dtype=torch.long).to(
+                        data_list[0].edge_index.device)
                     x = global_add_pool(x, batch, size=1)
                 else:
-                    x = torch.zeros(1, self.hidden).to(data_list[0].edge_index.device)
+                    x = torch.zeros(1, self.hidden).to(
+                        data_list[0].edge_index.device)
                 emb = torch.cat((emb, x), dim=0)
             if emb.shape[0] == 0:
                 continue
             else:
                 int_emb[num, :emb.shape[0], :] = emb
             num += 1
-            graph.x = emb  
-            new_data_list.append(graph) 
+            graph.x = emb
+            new_data_list.append(graph)
         if len(data_list) < 1:
             res = torch.zeros(1, 1).to(data_list[0].edge_index.device)
         else:
@@ -167,13 +177,16 @@ class localGNN_K4(torch.nn.Module):
             res = torch.squeeze(res, dim=-1)
         if len(new_data_list) < 1:
             res = torch.zeros(1, 1).to(data_list[0].edge_index.device)
-            new_edge_index = torch.zeros(2, 0).to(data_list[0].edge_index.device)
+            new_edge_index = torch.zeros(2, 0).to(
+                data_list[0].edge_index.device)
             new_edge_index = new_edge_index.to(torch.long)
-            batch = Batch.from_data_list([Data(x=torch.zeros(1, self.hidden).to(data_list[0].edge_index.device), edge_index=new_edge_index)])
+            batch = Batch.from_data_list([Data(x=torch.zeros(1, self.hidden).to(
+                data_list[0].edge_index.device), edge_index=new_edge_index)])
         else:
             batch = Batch.from_data_list(new_data_list)
         return batch, res
-    
+
+
 class localGNN_C4(torch.nn.Module):
     def __init__(self, num_layers, hidden):
         super(localGNN_C4, self).__init__()
@@ -201,19 +214,21 @@ class localGNN_C4(torch.nn.Module):
                 x = self.conv2(x, edge_index)
                 x = self.fc0(x)
                 x = F.gelu(x)
-                if len(x)>0:
-                    batch = torch.zeros(len(x), dtype=torch.long).to(data_list[0].edge_index.device)
+                if len(x) > 0:
+                    batch = torch.zeros(len(x), dtype=torch.long).to(
+                        data_list[0].edge_index.device)
                     x = global_add_pool(x, batch, size=1)
                 else:
-                    x = torch.zeros(1, self.hidden).to(data_list[0].edge_index.device)
+                    x = torch.zeros(1, self.hidden).to(
+                        data_list[0].edge_index.device)
                 emb = torch.cat((emb, x), dim=0)
             if emb.shape[0] == 0:
                 continue
             else:
                 int_emb[num, :emb.shape[0], :] = emb
             num += 1
-            graph.x = emb  
-            new_data_list.append(graph) 
+            graph.x = emb
+            new_data_list.append(graph)
         if len(data_list) < 1:
             res = torch.zeros(1, 1).to(data_list[0].edge_index.device)
         else:
@@ -222,13 +237,16 @@ class localGNN_C4(torch.nn.Module):
             res = torch.squeeze(res, dim=-1)
         if len(new_data_list) < 1:
             res = torch.zeros(1, 1).to(data_list[0].edge_index.device)
-            new_edge_index = torch.zeros(2, 0).to(data_list[0].edge_index.device)
+            new_edge_index = torch.zeros(2, 0).to(
+                data_list[0].edge_index.device)
             new_edge_index = new_edge_index.to(torch.long)
-            batch = Batch.from_data_list([Data(x=torch.zeros(1, self.hidden).to(data_list[0].edge_index.device), edge_index=new_edge_index)])
+            batch = Batch.from_data_list([Data(x=torch.zeros(1, self.hidden).to(
+                data_list[0].edge_index.device), edge_index=new_edge_index)])
         else:
             batch = Batch.from_data_list(new_data_list)
         return batch, res
-    
+
+
 class localGNN_tailed_triangle(torch.nn.Module):
     def __init__(self, num_layers, hidden):
         super(localGNN_tailed_triangle, self).__init__()
@@ -251,24 +269,26 @@ class localGNN_tailed_triangle(torch.nn.Module):
                 subgraph = sub_graphs[key]
                 edge_index = subgraph.edge_index
                 subgraph.x = torch.ones(
-                    [subgraph.num_nodes, 1]).to(edge_index.device)
+                    [max_nodes, 1]).to(edge_index.device)
                 x = self.conv1(subgraph.x, edge_index)
                 x = self.conv2(x, edge_index)
                 x = self.fc0(x)
                 x = F.gelu(x)
-                if len(x)>0:
-                    batch = torch.zeros(len(x), dtype=torch.long).to(data_list[0].edge_index.device)
+                if len(x) > 0:
+                    batch = torch.zeros(len(x), dtype=torch.long).to(
+                        data_list[0].edge_index.device)
                     x = global_add_pool(x, batch, size=1)
                 else:
-                    x = torch.zeros(1, self.hidden).to(data_list[0].edge_index.device)
+                    x = torch.zeros(1, self.hidden).to(
+                        data_list[0].edge_index.device)
                 emb = torch.cat((emb, x), dim=0)
             if emb.shape[0] == 0:
                 continue
             else:
                 int_emb[num, :emb.shape[0], :] = emb
             num += 1
-            graph.x = emb  
-            new_data_list.append(graph) 
+            graph.x = emb
+            new_data_list.append(graph)
         if len(data_list) < 1:
             res = torch.zeros(1, 1).to(data_list[0].edge_index.device)
         else:
@@ -277,9 +297,11 @@ class localGNN_tailed_triangle(torch.nn.Module):
             res = torch.squeeze(res, dim=-1)
         if len(new_data_list) < 1:
             res = torch.zeros(1, 1).to(data_list[0].edge_index.device)
-            new_edge_index = torch.zeros(2, 0).to(data_list[0].edge_index.device)
+            new_edge_index = torch.zeros(2, 0).to(
+                data_list[0].edge_index.device)
             new_edge_index = new_edge_index.to(torch.long)
-            batch = Batch.from_data_list([Data(x=torch.zeros(1, self.hidden).to(data_list[0].edge_index.device), edge_index=new_edge_index)])
+            batch = Batch.from_data_list([Data(x=torch.zeros(1, self.hidden).to(
+                data_list[0].edge_index.device), edge_index=new_edge_index)])
         else:
             batch = Batch.from_data_list(new_data_list)
         return batch, res